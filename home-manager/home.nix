1072b3dcc9d3c89b0a8102ed57ea293b7543cf4d
px <m@mamoco.se>
2024-06-01 21:54:02 +0200

    2024-06-01T21:54:02+02:00

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -14,7 +14,7 @@
 
     # You can also split up your configuration and import pieces of it here:
     # ./nvim.nix
-    ./fish.nix
+    #./fish.nix
   ];
 
   nixpkgs = {
@@ -141,6 +141,9 @@
 
     # nosqlite?
     sqlite
+
+    #godot
+    godot_4
   ];
 
 
