8ff368a9e4e94b314e21347939afd23a309c3ff0
px <m@mamoco.se>
2024-06-05 18:53:44 +0200

    2024-06-05T18:53:44+02:00

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -187,18 +187,6 @@ in
     userEmail = "magols@gmail.com";
   };
 
-  # starship - an customizable prompt for any shell
-  programs.starship = {
-    enable = true;
-    # custom settings
-    settings = {
-      add_newline = false;
-      aws.disabled = true;
-      gcloud.disabled = true;
-      line_break.disabled = true;
-    };
-  };
-
   programs.neovim = {
     enable = true;
     defaultEditor = true;
