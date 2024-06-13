4b86cf259b855c47b80e2bb4058d4aa3074da732
px <m@mamoco.se>
2024-06-13 15:49:51 +0200

    2024-06-13T15:49:43+02:00

--- nixos/nixos/configuration.nix
+++ nixos/nixos/configuration.nix
@@ -218,7 +218,7 @@
       timerConfig = {
         wantedBy = [ "timers.target" ];
         partOf = [ "test_systemd_timers.service" ];
-        OnCalendar = [ "3h" ];
+        OnCalendar = [ "3hours" ];
       };
     };
 
