3e455be2606bd12f37945630fa842900b248a362
px <m@mamoco.se>
2024-06-16 20:00:50 +0200

    Created by cron @ 2024-06-16T20:00:50+02:00

--- nixos/modules/terminals/foot.nix
+++ nixos/modules/terminals/foot.nix
@@ -3,18 +3,24 @@
     url = "https://raw.githubusercontent.com/catppuccin/foot/009cd57bd3491c65bb718a269951719f94224eb7/catppuccin-mocha.conf";
     hash = "sha256-plQ6Vge6DDLj7cBID+DRNv4b8ysadU2Lnyeemus9nx8=";
   };
+  tokyonightStorm = pkgs.fetchurl {
+    url = "https://codeberg.org/dnkl/foot/raw/branch/master/themes/tokyonight-storm";
+    hash = "sha256-V6IvefUH5CkbcAv868Ag0m4egIctqCmQFVtwwQByg3U=";
+  };
 in {
   programs.foot = {
     enable = true;
     settings = {
       main = {
-        include = "${catppuccinDrv}";
+        include = "${tokyonightStorm}";
         "font" = "JetBrains Mono Nerd Font:size=10";
         "shell" = "fish";
+        "term" = "xterm-256color";
       };
       "scrollback" = {
         "lines" = 10000;
       };
+
       # https://codeberg.org/dnkl/foot/raw/branch/master/foot.ini
     };
   };
--- nvim/init.lua
+++ nvim/init.lua
@@ -1,14 +1,15 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
-vim.g.clipboard = {
-  name = 'OSC 52',
-  copy = {
-    ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
-    ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
-  },
-  paste = {
-    ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
-    ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
-  },
-}
+-- vim.g.clipboard = {
+-- name = 'OSC 52',
+--  copy = {
+--     ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
+--     ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+--   },
+--  paste = {
+--    ['+'] = require('vim.ui.clipboard.osc52').copy('+'),
+--      ['*'] = require('vim.ui.clipboard.osc52').copy('*'),
+--    },
+-- }
+vim.api.nvim_set_option("clipboard","unnamedplus") 
