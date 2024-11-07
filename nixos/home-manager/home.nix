3e2749f0db04610bff547f9f7160971ec41df805
px <m@mamoco.se>
2024-11-07 19:21:20 +0100

    Created by cron @ 2024-11-07T19:21:20+01:00

--- nixos/home-manager/home.nix
+++ nixos/home-manager/home.nix
@@ -101,6 +101,7 @@ in
     socat # replacement of openbsd-netcat
     nmap # A utility for network discovery and security auditing
     ipcalc  # it is a calculator for the IPv4/v6 addresses
+    mkcert # maek local certs n shit
 
     # misc
     file
