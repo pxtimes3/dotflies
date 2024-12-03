f3d52635cef40a6a193ae54c208c7faa5eff19f1
px <m@mamoco.se>
2024-12-03 13:15:54 +0100

    Created by cron @ 2024-12-03T13:15:54+01:00

--- nixos/nixos/configuration.nix
+++ nixos/nixos/configuration.nix
@@ -290,6 +290,18 @@
 #    fsType = "nfs";
 #  };
 
+  programs.nix-ld.enable = true;
+  programs.nix-ld.libraries = with pkgs; [
+    stdenv.cc.cc
+    zlib
+    glib
+    # Common libraries that might be needed
+    openssl
+    curl
+    glibc
+    icu
+  ];
+
 
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   system.stateVersion = "23.11";
