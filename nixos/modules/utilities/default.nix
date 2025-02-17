a43b14822eee70bdaa91b2b159a8248c0c0c0b0c
installer <installer@gmail.com>
2025-02-17 14:04:24 +0000

    `stuff`

--- nixos/modules/utilities/default.nix
+++ nixos/modules/utilities/default.nix
@@ -1,42 +1,35 @@
-# /etc/nixos/modules/utilities/default.nix
-{ config, pkgs, ... }: {
+{ config, pkgs, ... }: let
+  volumectl = pkgs.writeScriptBin "volumectl" (builtins.readFile ./scripts/volumectl);
+  lightctl = pkgs.writeScriptBin "lightctl" (builtins.readFile ./scripts/lightctl);
+  wofiEmoji = pkgs.writeScriptBin "wofi-emoji" (builtins.readFile ./scripts/wofi-emoji);
+  hyprFixSpecial = pkgs.writeScriptBin "hypr-fix-special" (builtins.readFile ./scripts/hypr-fix-special);
+  rebuild = pkgs.writeScriptBin "rebuild" (builtins.readFile ./scripts/rebuild);
+in {
   environment.systemPackages = with pkgs; [
     # Audio control
-    pamixer        # Backend for volume control
-    playerctl      # Media player control
-    
+    pamixer
+    playerctl
+
     # Brightness
-    light          # Alternative brightness control
-    
+    light
+
     # System utilities
-    wofi           # Application launcher
-    wl-clipboard   # Clipboard management
-    grim           # Screenshot utility
-    slurp          # Area selection
-    jq             # JSON processor
-    socat          # Socket utility
-    
+    wofi
+    wl-clipboard
+    grim
+    slurp
+    jq
+    socat
+
     # File management
-    ranger        # TUI file manager
-    xfce.thunar        # GUI file manager
+    ranger
+    xfce.thunar
+
+    # Our custom scripts
+    volumectl
+    lightctl
+    wofiEmoji
+    hyprFixSpecial
+    rebuild
   ];
-
-  environment.etc = {
-    "utilities/volumectl".source = ./scripts/volumectl;
-    "utilities/lightctl".source = ./scripts/lightctl;
-    "utilities/wofi-emoji".source = ./scripts/wofi-emoji;
-    "utilities/hypr-fix-special".source = ./scripts/hypr-fix-special;
-    "utilities/rebuild".source = ./scripts/rebuild;
-  };
-
-  # Modified activation script
-  system.activationScripts.utilities.text = ''
-    mkdir -p /home/px/.local/bin
-    for util in /etc/utilities/*; do
-      name=$(basename "$util")
-      cp -f "$util" "/home/px/.local/bin/$name"
-      chmod +x "/home/px/.local/bin/$name"
-    done
-    chown -R px:users /home/px/.local/bin
-  '';
 }
